08ae247a90734767f28c5f796cdbdca8fd72a4a4
James Meng <james.meng@shopify.com>
2023-07-31 18:45:06 -0400

    End to End Agent Implementation

--- ice_breaker.py
+++ ice_breaker.py
@@ -7,10 +7,12 @@ from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 
-name = "Harrison Chase"
-if __name__ == "__main__":
-    print("Hello LangChain!")
+from output_parser import person_intel_parser
 
+name = "Harley Finklestein"
+
+
+def ice_break(name):
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
@@ -18,20 +20,33 @@ if __name__ == "__main__":
     tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
 
     summary_template = """
-         given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person from I want you to create:
-         1. a short summary
-         2. two interesting facts about them
-         3. A topic that may interest them
-         4. 2 creative Ice breakers to open a conversation with them 
-     """
+            given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person from I want you to create:
+            1. a short summary
+            2. two interesting facts about them
+            3. A topic that may interest them
+            4. 2 creative Ice breakers to open a conversation with them
+            \n{format_instructions}
+        """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=1, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    print(result)
+    print(person_intel_parser.parse(result))
+
+    return person_intel_parser.parse(result)
+
+
+if __name__ == "__main__":
+    print("Hello LangChain!")
+    result = ice_break("Harley Finklestein")
